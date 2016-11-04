033713b646e522e1af426757e38927c926ddce06
root <root@pontus@gmail.com>
2016-11-04 23:49:24 +0100

    test

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -329,7 +329,7 @@ function wp_mail( $to, $subject, $message, $headers = '', $attachments = array()
 		}
 
 		//$from_email = 'wordpress@' . $sitename;
-		$from_email = 'wordpress@http://138.68.143.251/';
+		$from_email = 'wordpress@138.68.143.251';
 	}
 
 	/**
